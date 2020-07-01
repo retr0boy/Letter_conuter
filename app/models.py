82900039fb39e7a8b3c0a46aa8670473fedb8248
Мирас Мұратханов <muratkanov_m0322@sm.nis.edu.kz>
2020-07-01 14:03:55 +0600

    added smtg

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
-    file_path = db.Column(db.String(128))
+    name = db.Column(db.String(64), index=True, unique=True)
+    file_path = db.Column(db.String(128), index=True, unique=True)
     
