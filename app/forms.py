6cf4be290d526b0e5191cac823d54b2409574b3e
Мирас Мұратханов <muratkanov_m0322@sm.nis.edu.kz>
2020-07-01 12:22:40 +0600

    added bootstrap and making my way to get database

--- app/forms.py
+++ app/forms.py
@@ -3,9 +3,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 
-
-
-class MyForm(FlaskForm):
+class Upload(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
     file = FileField('text file', validators=[FileRequired(),FileAllowed(['txt'], 'text files only!')])
     submit = SubmitField('Send')
--- app/routes.py
+++ app/routes.py
@@ -1,35 +1,41 @@
 from app import app
-from flask import render_template, redirect
-from app.forms import MyForm
+from flask import render_template, url_for
+from app.forms import Upload
+from config import config
 from werkzeug.utils import secure_filename
 import os
-import secrets
-from chardet.universaldetector import UniversalDetector
 from mylib import count_ru_letters, detect_encoding
-
+import plotly as py
+import plotly.graph_objects as go
 
 @app.route('/')
 @app.route('/index')
-def hello():
-    countries = {'Rusea':'moskorowa','washdc':'usausa','kaza':'asta'}
-    return render_template('index.html', data = countries)
+def index():
+    data='Welcome username'
+    dictionary={'Россия':'Москва', 'Казахстан':'Нур-Султан', 'Италия':'Рим'}
+    return render_template('index.html', dictionary=dictionary, title=data)
 
 
-@app.route('/upload', methods=['GET', 'POST'])
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
+    form = Upload()
+    if form.validate_on_submit():
+        f = form.file.data
+        filename = secure_filename(f.filename)
+        files_dir = os.path.join(os.getcwd(),'files')
+        html_dir = os.path.join(files_dir,'html')
+        filename = os.path.join(files_dir, filename)
 
-    form = MyForm()
-    if form.validate_onsubmit ():
-
-        , file_ext = os.path.splitext(form.file.data.filename)
-        files_dir=os.path.join(os.getcwd(), 'files')
-        filename=os.path.join(files_dir, secrets.token_hex(nbytes=16)+file_ext)
-        if 'files' not in os.listdir(os.getcwd()):
+        if not os.path.exists(files_dir):
             os.mkdir(files_dir)
-        form.file.data.save(filename)
-        f = open(filename, "r", encoding=detect_encoding(filename)['encoding'])
+        if not os.path.exists(html_dir):
+            os.mkdir(html_dir)
+        f.save(filename)
+        f=open(filename,'r',encoding=detect_encoding(filename)['encoding'])
         text=f.read()
-        count_ru_letters(text)
-        return render_template('index.html',title = form.name.data, text=text)
+        f.close()
+        result=count_ru_letters(text) 
+        fig = go.Figure([go.Bar(x=list(result.keys()), y=list(result.values()))])
+        py.offline.plot(fig,filename=os.path.join(html_dir,filename+'_'+'counter.html'),auto_open=True)          
+        return render_template('index.html')
     return render_template('upload.html', form=form)
